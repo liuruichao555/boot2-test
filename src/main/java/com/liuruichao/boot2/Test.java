ebfc46c985cf32cf7c5362b3cd074065a8bb42eb
liuruichao <liuruichao555@126.com>
2021-08-04 14:25:31 +0800

    test

--- src/main/java/com/liuruichao/boot2/Test.java
+++ src/main/java/com/liuruichao/boot2/Test.java
@@ -11,7 +11,11 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
  */
 public class Test {
     public static void main(String[] args) {
-
+        Student student = new Student();
+        student.setId(1);
+        student.setName("liuruichao");
+        student.setAge(20);
+        System.out.println("student = " + student);
     }
 
     private static class Student {
@@ -20,6 +24,33 @@ public class Test {
         private String name;
 
         private int age;
+
+        public int getId() {
+            return id;
+        }
+
+        public Student setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Student setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Student setAge(int age) {
+            this.age = age;
+            return this;
+        }
 
         @Override
         public boolean equals(Object o) {
